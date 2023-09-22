dc88562ca84585606ed09b0d7834a242dc3e4a52
yuziem14 <yuri.fnunes@gmail.com>
2023-09-22 15:27:47 -0300

    week 02 project core requirements fullfiled

--- w02/w02-meal-price-calculator.py
+++ w02/w02-meal-price-calculator.py
@@ -17,3 +17,19 @@ print()
 # Calculate Sub Total
 subtotal = (child_meal_price * number_of_children) + (adult_meal_price * number_of_adults)
 print(f'Subtotal: ${subtotal:.2f}')
+print()
+
+# Receive Tax Rate and calculate the sales tax as also the total price (subtotal plus tax)
+sales_tax_rate = int(input('What is the sales tax rate? '))
+sales_tax = subtotal * (sales_tax_rate / 100)
+total = subtotal + sales_tax
+
+print(f'Sales Tax: ${sales_tax:.2f}')
+print(f'Total: ${total:.2f}')
+print()
+
+# Receive the payment amount, calculate and display the change amount
+payment_amount = float(input('What is the payment amount? '))
+change_value = payment_amount - total
+
+print(f'Change: ${change_value:.2f}')
