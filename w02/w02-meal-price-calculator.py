99be0855ef43bdd153396be5d72923f65af91feb
yuziem14 <yuri.fnunes@gmail.com>
2023-09-22 15:40:53 -0300

    w02-exceed-requirement: change meal price calculator to calculate the users tip

--- w02/w02-meal-price-calculator.py
+++ w02/w02-meal-price-calculator.py
@@ -2,6 +2,8 @@
 @author Yuri Nunes
 
 Week 02 Project: Meal Price Calculator
+
+Exceed Requirement: Ask the user if he wants to give a tip and calculate the total that must be paid and the change
 '''
 
 print('Let\'s calculate the price of your meal. Answer the following:')
@@ -15,21 +17,27 @@ number_of_adults = int(input('How many adults are there? '))
 print()
 
 # Calculate Sub Total
-subtotal = (child_meal_price * number_of_children) + (adult_meal_price * number_of_adults)
-print(f'Subtotal: ${subtotal:.2f}')
+total_price = (child_meal_price * number_of_children) + (adult_meal_price * number_of_adults)
+print(f'Subtotal: ${total_price:.2f}')
 print()
 
-# Receive Tax Rate and calculate the sales tax as also the total price (subtotal plus tax)
+# Receive Tax Rate and calculate the sales tax, add the sales tax with the subtotal and display it
 sales_tax_rate = int(input('What is the sales tax rate? '))
-sales_tax = subtotal * (sales_tax_rate / 100)
-total = subtotal + sales_tax
+sales_tax = total_price * (sales_tax_rate / 100)
+total_price += sales_tax
 
 print(f'Sales Tax: ${sales_tax:.2f}')
-print(f'Total: ${total:.2f}')
+print(f'Subtotal: ${total_price:.2f}')
+print()
+
+# Ask if the user wants to give a tip and add to the subtotal
+tip_value = float(input('How much you want to give for tip? (if none, type 0) '))
+total_price += tip_value
+print(f'Total: ${total_price:.2f}')
 print()
 
 # Receive the payment amount, calculate and display the change amount
 payment_amount = float(input('What is the payment amount? '))
-change_value = payment_amount - total
+change_value = payment_amount - total_price
 
 print(f'Change: ${change_value:.2f}')
