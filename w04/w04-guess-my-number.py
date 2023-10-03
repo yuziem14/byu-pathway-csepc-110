26bf1e28ff2c1a10256b2c6f7d5bce18820425ca
yuziem14 <yuri.fnunes@gmail.com>
2023-10-03 11:49:52 -0300

    w04 guess my number game finished

--- w04/w04-guess-my-number.py
+++ w04/w04-guess-my-number.py
@@ -8,23 +8,35 @@ Authors
 @Randy Munõz
 '''
 
-# Import OS and Random Library
-import os
+# Import random and os Library
 import random
+import os
 
+MIN_NUMBER = 0
+MAX_NUMBER = 100
 
-magic_number = int(input('> What is the magic number to guess? '))
-# User guess will always start different from the magic number
-user_guess = magic_number + 1
+keep_playing = True
 
-os.system('cls' if os.name == 'nt' else 'clear')
+while keep_playing:
+    magic_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+    # User guess will always start different from the magic number
+    user_guess = magic_number + 1
+    guess_tracker = 0
 
-while(magic_number != user_guess):
-    user_guess = int(input('> What\'s the magic number? '))
+    while(magic_number != user_guess):
+        user_guess = int(input('> What\'s the magic number? '))
+        guess_tracker += 1
 
-    if user_guess < magic_number:
-        print('Lower')
-    elif user_guess > magic_number:
-        print('Higher')
-    else:
-        print('You guessed it!')
+        if magic_number < user_guess:
+            print('Lower')
+        elif magic_number > user_guess:
+            print('Higher')
+        else:
+            print('You guessed it!')
+            print(f'Number Of Guesses: {guess_tracker}')
+            print()
+
+    keep_playing = input('> Play Again (Yes or No)? ').lower() == 'yes'
+
+    # Clean the terminal after finishing the game
+    os.system('cls' if os.name == 'nt' else 'clear')
