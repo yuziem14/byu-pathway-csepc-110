78366c174c3e0af1c15d20d21a948ad081db4dba
yuziem14 <yuri.fnunes@gmail.com>
2023-10-14 21:19:15 -0300

    w05 project: all requirements done

--- w05/w05-shopping-cart.py
+++ w05/w05-shopping-cart.py
@@ -3,7 +3,7 @@
 
 Week 05 Project: Shopping Cart
 
-Exceed Requirement: None
+Exceed Requirement: Add quantity of items
 '''
 
 # Store Options ins Constants
@@ -16,7 +16,9 @@ MENU_OPTIONS = [f'{ADD_ITEM}. Add item', f'{VIEW_CART}. View cart', f'{REMOVE_IT
 
 # Initialize Variables
 selected_option = '0'
-cart = []
+cart_items = []
+prices = []
+items_quantity = []
 
 print('Welcome to the Shopping Cart Program!')
 
@@ -29,16 +31,49 @@ while(selected_option != QUIT_PROGRAM):
 
     if(selected_option == ADD_ITEM):
         item_name = input('> What item would you like to add? ').lower()
+        item_price = float(input(f'> What is the price of \'{item_name}\'? '))
+        item_quantity = int(input(f'> What is the quantity of \'{item_name}\'? '))
+
+        if(item_quantity < 1):
+            print('Sorry. The quantity minimum is 1. If this is a mistake, please remove the item.')
+            item_quantity = 1
+
+        cart_items.append(item_name)
+        prices.append(item_price)
+        items_quantity.append(item_quantity)
+        
         print(f'\'{item_name}\' has been added to the cart.')
-        cart.append(item_name)
     elif(selected_option == VIEW_CART):
         print('The contents of the shopping cart are:')
-        for index in range(len(cart)):
-            print(f'{index+1}. {cart[index].capitalize()}')
+
+        for index in range(len(cart_items)):
+            item_number = index + 1
+            item_name = cart_items[index].capitalize()
+            item_price = prices[index]
+            item_quantity = items_quantity[index]
+            total_price = item_price * item_quantity
+
+            print(f'{item_number}. {item_name} - ${item_price:.2f} x {item_quantity} = ${total_price:.2f}')
     elif(selected_option == REMOVE_ITEM):
-        print()
+        item_number = int(input('> Which item would you like to remove? '))
+        
+        if(item_number < 1 or item_number > len(cart_items)):
+            print('Sorry, that is not a valid item number.\n')
+            continue
+
+        index = item_number-1
+        cart_items.pop(index)
+        prices.pop(index)
+        items_quantity.pop(index)
+        print('Item removed.')
     elif(selected_option == COMPUTE_TOTAL):
-        print()
+        total_cart_price = 0
+        for index in range(len(cart_items)):
+            price = prices[index]
+            item_quantity = items_quantity[index]
+            total_cart_price += item_quantity * price
+        
+        print(f'The total price of the items in the shopping cart is ${total_cart_price:.2f}')
     elif(selected_option == QUIT_PROGRAM):
         print('Thank you. Goodbye.')
     else:
